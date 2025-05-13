298e302acf57484b1b2de902636470d525569593
etu <leovirhaege@gmail.com>
2025-05-13 12:15:09 +0200

    tout est placé et dans des cases

--- JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/HelloApplication.java
+++ JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/HelloApplication.java
@@ -3,10 +3,11 @@ package com.example.jeuduloup2;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
@@ -16,12 +17,45 @@ public class HelloApplication extends Application {
     private boolean loupPlace = false;
     private boolean existSortie = false;
 
+    private StackPane creerBoutonImage(Image image, Runnable action) {
+        ImageView icone = new ImageView(image);
+        icone.setFitWidth(140);
+        icone.setFitHeight(140);
+
+
+        StackPane container = new StackPane(icone);
+        container.setPrefSize(150, 150);
+        container.setStyle("""
+            -fx-background-color: rgba(255, 255, 255, 0.1);
+            -fx-border-color: white;
+            -fx-border-width: 2px;
+            -fx-border-radius: 15px;
+            -fx-background-radius: 15px;
+            -fx-cursor: hand;
+        """);
+
+        container.setOnMouseClicked(e -> action.run());
+        return container;
+    }
+
     @Override
     public void start(Stage primaryStage) {
         Grille grille = new Grille(10, 10);
         grille.miseEnPlace();
 
-        // Chargement des images
+        StackPane container = new StackPane();
+        container.setPrefSize(150,500);
+        container.setStyle("""
+            -fx-background-color: rgba(255, 255, 255, 0.1);
+            -fx-border-color: white;
+            -fx-border-width: 2px;
+            -fx-border-radius: 15px;
+            -fx-background-radius: 15px;
+            -fx-cursor: hand;
+        """);
+        VBox cont = new VBox(container);
+        cont.setAlignment(Pos.CENTER_RIGHT);
+        cont.setPrefSize(150,500);
         Image imgFondHerbe = new Image(getClass().getResourceAsStream("/com/example/jeuduloup2/FondHerbe.png"));
         Image imgHerbe = new Image(getClass().getResourceAsStream("/com/example/jeuduloup2/herbe.png"));
         Image imgRocher = new Image(getClass().getResourceAsStream("/com/example/jeuduloup2/rocher.png"));
@@ -29,6 +63,8 @@ public class HelloApplication extends Application {
         Image imgLoup = new Image(getClass().getResourceAsStream("/com/example/jeuduloup2/loup.png"));
         Image imgMarguerite = new Image(getClass().getResourceAsStream("/com/example/jeuduloup2/marguerite.png"));
         Image imgCactus = new Image(getClass().getResourceAsStream("/com/example/jeuduloup2/cactus.png"));
+        Image imgSupprimer = new Image(getClass().getResourceAsStream("/com/example/jeuduloup2/supprimer.png"));
+        Image imgSortie = new Image(getClass().getResourceAsStream("/com/example/jeuduloup2/sortie.png"));
 
         GridPane grid = new GridPane();
         grid.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
@@ -74,7 +110,20 @@ public class HelloApplication extends Application {
 
                     if (modePlacement.equals("mouton") && !moutonPlace) {
                         boolean estBordure = (x == 0 || y == 0 || x == nbColonnes - 1 || y == nbLignes - 1);
-                        if (!estRocher && !estBordure) {
+                        if (estRocher){
+                            Text text = new Text("Il y a un rocher !");
+                            text.setFont(new Font("Arial", 17));
+                            text.setStyle("-fx-text-fill: white;");
+                            container.getChildren().add(text);
+                        }
+                        else if (grille.getElements().stream().anyMatch(e -> e.getX() == x && e.getY() == y && e instanceof Loup)) {
+                            Text text = new Text("Il y a déjà un loup ici !");
+                            text.setFont(new Font("Arial", 17));
+                            text.setStyle("-fx-text-fill: white;");
+                            container.getChildren().add(text);
+                        }
+
+                        else if (!estRocher && !estBordure) {
                             cell.getChildren().clear();
                             grille.getElements().removeIf(e -> e.getX() == x && e.getY() == y && !(e instanceof Rocher));
 
@@ -87,6 +136,7 @@ public class HelloApplication extends Application {
                             moutonPlace = true;
                             modePlacement = null;
                         }
+
                     } else if (modePlacement.equals("loup") && !loupPlace) {
                         boolean estBordure = (x == 0 || y == 0 || x == nbColonnes - 1 || y == nbLignes - 1);
                         if (!estRocher && !estBordure) {
@@ -113,7 +163,6 @@ public class HelloApplication extends Application {
                             cell.getChildren().add(marguerite);
                             grille.getElements().add(new Marguerite(x, y));
                         }
-
                     } else if (modePlacement.equals("cactus")) {
                         boolean estBordure = (x == 0 || y == 0 || x == nbColonnes - 1 || y == nbLignes - 1);
                         if (!estRocher && !estBordure) {
@@ -125,7 +174,6 @@ public class HelloApplication extends Application {
                             cell.getChildren().add(cactus);
                             grille.getElements().add(new Cactus(x, y));
                         }
-
                     } else if (modePlacement.equals("supprimer")) {
                         if (!estRocher) {
                             grille.getElements().removeIf(e -> e.getX() == x && e.getY() == y && (e instanceof Mouton || e instanceof Loup));
@@ -143,9 +191,6 @@ public class HelloApplication extends Application {
                                 loupPlace = false;
                             }
                         }
-
-
-
                     } else if (modePlacement.equals("sortie")) {
                         boolean estBordure = (x == 0 || y == 0 || x == nbColonnes - 1 || y == nbLignes - 1);
                         if (existSortie) {
@@ -167,37 +212,32 @@ public class HelloApplication extends Application {
                             existSortie = true;
                         }
                     }
-
-
                 });
 
                 grid.add(cell, j, i);
             }
         }
 
-        // Boutons
-        Button boutonMouton = new Button("Placer le mouton");
-        boutonMouton.setOnAction(e -> modePlacement = "mouton");
 
-        Button boutonLoup = new Button("Placer le loup");
-        boutonLoup.setOnAction(e -> modePlacement = "loup");
 
-        Button boutonMarguerite = new Button("Placer une marguerite");
-        boutonMarguerite.setOnAction(e -> modePlacement = "marguerite");
 
-        Button boutonCactus = new Button("Placer un cactus");
-        boutonCactus.setOnAction(e -> modePlacement = "cactus");
+        StackPane boutonMouton = creerBoutonImage(imgMouton, () -> modePlacement = "mouton");
+        StackPane boutonLoup = creerBoutonImage(imgLoup, () -> modePlacement = "loup");
+        StackPane boutonMarguerite = creerBoutonImage(imgMarguerite, () -> modePlacement = "marguerite");
+        StackPane boutonCactus = creerBoutonImage(imgCactus, () -> modePlacement = "cactus");
+        StackPane boutonSupprimer = creerBoutonImage(imgSupprimer, () -> modePlacement = "supprimer");
+        StackPane boutonSortie = creerBoutonImage(imgSortie, () -> modePlacement = "sortie");
 
-        Button boutonSupprimer = new Button("Supprimer");
-        boutonSupprimer.setOnAction(e -> modePlacement = "supprimer");
-
-        Button boutonSortie = new Button("Placer la sortie");
-        boutonSortie.setOnAction(e -> modePlacement = "sortie");
-
-        VBox menu = new VBox(20, boutonMouton, boutonLoup, boutonCactus, boutonMarguerite, boutonSupprimer, boutonSortie);
+        VBox menu = new VBox(20, boutonMouton, boutonLoup, boutonMarguerite);
         menu.setAlignment(Pos.CENTER);
-
-        HBox rootContent = new HBox(50, grid, menu);
+        VBox menu1 = new VBox(20, boutonCactus, boutonSupprimer, boutonSortie);
+        menu1.setAlignment(Pos.CENTER);
+        menu.setSpacing(50);
+        menu1.setSpacing(50);
+        HBox toutmenu = new HBox(20, menu, menu1);
+        toutmenu.setAlignment(Pos.CENTER);
+        HBox rootContent = new HBox(50,cont, grid, toutmenu);
+        rootContent.setSpacing(200);
         rootContent.setAlignment(Pos.CENTER);
 
         ImageView background = new ImageView(imgFondHerbe);
