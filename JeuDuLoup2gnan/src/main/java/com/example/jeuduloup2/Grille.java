a7c73f3c1a5b689abdd11af67fe8de89db71efd8
etu <chakourabdelwaheb20@gmail.com>
2025-05-14 14:49:08 +0200

    Grille modifié HEHEE

--- JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/Grille.java
+++ JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/Grille.java
@@ -1,26 +1,27 @@
 package com.example.jeuduloup2;
 
-import java.util.ArrayList;
-
 public class Grille {
-    // Changer en matrice /!\ helloapplication va devoir changer
-    private ArrayList<Elements> elements= new ArrayList<>();
+    private Elements[][] elements;
     private int nbLignes;
     private int nbColonnes;
 
     public Grille(int nbLignes, int nbColonnes) {
-        this.nbLignes = nbLignes;
-        this.nbColonnes = nbColonnes;
+        this.nbLignes = nbLignes + 2; // Ajout des bordures
+        this.nbColonnes = nbColonnes + 2;
+        this.elements = new Elements[this.nbColonnes][this.nbLignes];
     }
 
-    public ArrayList<Elements> getElements()
-    {
-        //nul, a changer (plutot get element x y comme matrice )
-        return elements;
+    public Elements getElement(int x, int y) {
+        return elements[x][y];
+    }
+
+    public void setElement(int x, int y, Elements e) {
+        elements[x][y] = e;
     }
     public Elements getElement(int x, int y){
         return elements.get(x+y*nbColonnes);
     }
+
     public int getNbLignes() {
         return nbLignes;
     }
@@ -30,43 +31,86 @@ public class Grille {
     }
 
     public void miseEnPlace() {
-        this.nbLignes += 2;
-        this.nbColonnes += 2;
-
-
-        for (int i = 0; i < this.nbColonnes; i++) {
-            elements.add(new Rocher(i, 0)); // haut
-            elements.add(new Rocher(i, nbLignes - 1));
-        }
-        for (int i = 1; i < this.nbLignes - 1; i++) {
-            elements.add(new Rocher(0, i)); // gauche
-            elements.add(new Rocher(nbColonnes - 1, i));
+        // Bordures haut et bas
+        for (int x = 0; x < nbColonnes; x++) {
+            elements[x][0] = new Rocher(x, 0);
+            elements[x][nbLignes - 1] = new Rocher(x, nbLignes - 1);
         }
 
+        // Bordures gauche et droite
+        for (int y = 1; y < nbLignes - 1; y++) {
+            elements[0][y] = new Rocher(0, y);
+            elements[nbColonnes - 1][y] = new Rocher(nbColonnes - 1, y);
+        }
 
-        for (int y = 1; y <= this.nbLignes - 2; y++) {
-            for (int x = 1; x <= this.nbColonnes - 2; x++) {
-                elements.add(new Herbe(x, y));
+        // Remplissage intérieur avec de l'herbe
+        for (int y = 1; y < nbLignes - 1; y++) {
+            for (int x = 1; x < nbColonnes - 1; x++) {
+                elements[x][y] = new Herbe(x, y);
             }
         }
     }
 
-    public void setSortie(int x, int y){
-        for (int i=0; i<this.elements.size(); i++){
-            if (this.elements.get(i).getX() == x && this.elements.get(i).getY() == y){
-                this.elements.set(i,new Sortie(x,y));
-            }
-        }
+    public void setSortie(int x, int y) {
+        elements[x][y] = new Sortie(x, y);
     }
-    public boolean sortieValide(){
-        for (int i=0; i<this.elements.size();i++){
-            if (this.elements.get(i) instanceof Sortie){
-                if ((this.elements.get(i).getX()==0 || this.elements.get(i).getX()==this.nbColonnes)&&(this.elements.get(i).getY()==0 || this.elements.get(i).getY()==nbLignes )){
-                    if ((this.elements.get(i).getX()==0 && this.elements.get(i).getY()==0) ||(this.elements.get(i).getX()==0 && this.elements.get(i).getY()==nbColonnes) || (this.elements.get(i).getX()==nbLignes && this.elements.get(i).getY()==nbColonnes) ||(this.elements.get(i).getX()==nbColonnes && this.elements.get(i).getY()==0)){
-                        return false;
+
+    public boolean sortieValide() {
+        for (int x = 0; x < nbColonnes; x++) {
+            for (int y = 0; y < nbLignes; y++) {
+                if (elements[x][y] instanceof Sortie) {
+                    boolean surBord = (x == 0 || x == nbColonnes - 1 || y == 0 || y == nbLignes - 1);
+                    boolean enCoin = (x == 0 && y == 0) ||
+                            (x == 0 && y == nbLignes - 1) ||
+                            (x == nbColonnes - 1 && y == 0) ||
+                            (x == nbColonnes - 1 && y == nbLignes - 1);
+                    if (surBord && !enCoin) {
+                        return true;
                     }
                 }
+            }
+        }
+        return false;
+    }
 
+    public boolean estConnexe() {
+        boolean[][] visite = new boolean[nbColonnes][nbLignes];
+
+        // Trouver une case Herbe pour démarrer
+        for (int y = 0; y < nbLignes; y++) {
+            for (int x = 0; x < nbColonnes; x++) {
+                if (elements[x][y] instanceof Herbe) {
+                    // Lancer le DFS
+                    explorer(x, y, visite);
+                    // Vérifier ensuite si tout est visité
+                    return verifierConnexite(visite);
+                }
+            }
+        }
+
+        // Aucune case Herbe trouvée
+        return false;
+    }
+
+    private void explorer(int x, int y, boolean[][] visite) {
+        if (x < 0 || x >= nbColonnes || y < 0 || y >= nbLignes) return;
+        if (visite[x][y]) return;
+        if (!(elements[x][y] instanceof Herbe || elements[x][y] instanceof Sortie)) return;
+
+        visite[x][y] = true;
+
+        explorer(x + 1, y, visite);
+        explorer(x - 1, y, visite);
+        explorer(x, y + 1, visite);
+        explorer(x, y - 1, visite);
+    }
+
+    private boolean verifierConnexite(boolean[][] visite) {
+        for (int y = 0; y < nbLignes; y++) {
+            for (int x = 0; x < nbColonnes; x++) {
+                if ((elements[x][y] instanceof Herbe || elements[x][y] instanceof Sortie) && !visite[x][y]) {
+                    return false;
+                }
             }
         }
         return true;
