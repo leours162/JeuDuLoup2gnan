78c2564c86343ca13b79b6aafd16e314943d8d61
Abdelwaheb Chakour <chakourabdelwaheb20@gmail.com>
2025-05-17 11:02:44 +0200

    Mise a jour

--- JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/Elements.java
+++ JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/Elements.java
@@ -5,33 +5,48 @@ public abstract class Elements {
     protected int y;
     protected boolean accessible;
 
-
+    
     public Elements(int x, int y) {
         this.x = x;
         this.y = y;
-        this.accessible = true;
+        this.accessible = true; 
     }
+
+    
     public Elements() {
         this.x = 0;
         this.y = 0;
+        this.accessible = true;
     }
+
+    
     public int getX() {
         return x;
     }
+
     public int getY() {
         return y;
     }
-    public void setX(int x) {
-        this.x = x;
-    };
-    public void setY(int y) {
-        this.y = y;
-    }
+
     public boolean isAccessible() {
         return accessible;
     }
-    public void pasDacces () {
+
+   
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setAccessible(boolean accessible) {
+        this.accessible = accessible;
+    }
+
+    
+    public void rendreInaccessible() {
         this.accessible = false;
     }
 }
-
