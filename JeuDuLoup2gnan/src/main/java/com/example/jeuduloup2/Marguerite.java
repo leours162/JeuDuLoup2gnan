68681470ec39e862e67a980cfc2d838ff3149860
Abdelwaheb Chakour <chakourabdelwaheb20@gmail.com>
2025-05-17 11:10:20 +0200

    Update Marguerite.java

--- JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/Marguerite.java
+++ JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/Marguerite.java
@@ -1,11 +1,8 @@
 package com.example.jeuduloup2;
 
-public class Marguerite extends Vegetaux{
-
+public class Marguerite extends Vegetaux {
     public Marguerite(int x, int y) {
         super(4, x, y);
     }
-
-
 }
 
