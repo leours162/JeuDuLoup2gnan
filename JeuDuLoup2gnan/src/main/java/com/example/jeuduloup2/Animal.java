72529de8dceb9133fd504b1dd1be8d4d2ceb1589
Abdelwaheb Chakour <chakourabdelwaheb20@gmail.com>
2025-05-17 11:04:42 +0200

    Update Animal.java

--- JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/Animal.java
+++ JeuDuLoup2gnan/src/main/java/com/example/jeuduloup2/Animal.java
@@ -1,36 +1,38 @@
 package com.example.jeuduloup2;
 
-import com.example.jeuduloup2.Elements;
-
 public abstract class Animal extends Elements {
     protected int vitesse;
     protected boolean vivant;
 
-    public Animal (int vitesse,int x, int y) {
-        super(x,y);
+    public Animal(int vitesse, int x, int y) {
+        super(x, y);
         this.vitesse = vitesse;
         this.vivant = true;
     }
 
-    public Animal(){
+    public Animal() {
+        super();
         this.vitesse = 0;
+        this.vivant = true;
     }
 
-    public int getVitesse(){
+    public int getVitesse() {
         return this.vitesse;
     }
-    public void setVitesse(int vitesse){
+
+    public void setVitesse(int vitesse) {
         this.vitesse = vitesse;
     }
-    public boolean estVivant(){
+
+    public boolean estVivant() {
         return this.vivant;
     }
-    public void mourir(boolean vivant){
+
+    public void mourir() {
         this.vivant = false;
     }
 
-
-    public void bouger (int x, int y){
+    public void bouger(int x, int y) {
         this.x = x;
         this.y = y;
     }
