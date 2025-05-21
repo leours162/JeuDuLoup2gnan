0ada9a81a3d214716e2f7d6a88bc8208b00b688e
BOLTZ Louis <boltzlouis44@gmail.com>
2025-05-21 16:45:49 +0200

    junit

---
+++ JeuDuLoup2gnan/src/test/java/com/example/jeuduloup2/GrilleTest.java
@@ -0,0 +1,12 @@
+package com.example.jeuduloup2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GrilleTest {
+
+    @Test
+    void lesDeplacements() {
+    }
+}
